803caf4d7a33c3d308c4ff90039debc67cd6b8ae
Jebitok <sharonkosgei4@gmail.com>
2021-03-02 12:32:33 +0300

    Add New Migration

--- db/migrate/20210302084759_add_user_to_events.rb
+++ db/migrate/20210302084759_add_user_to_events.rb
@@ -1,5 +1,5 @@
 class AddUserToEvents < ActiveRecord::Migration[6.1]
   def change
-    add_reference :events, :user, null: false, foreign_key: true
+    add_reference :events, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_164501) do
+ActiveRecord::Schema.define(version: 2021_03_02_084759) do
 
   create_table "events", force: :cascade do |t|
     t.text "event"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_03_01_164501) do
     t.text "invite"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_03_01_164501) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
