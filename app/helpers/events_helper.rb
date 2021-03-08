1162e4ca69f79442ad668b554247274c76573972
Jebitok <sharonkosgei4@gmail.com>
2021-03-08 14:38:26 +0300

    Add Helpers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,24 @@
 module EventsHelper
+    def past_events(past_events)
+        if past_events.all.size.zero?
+            render 'no_past_event'
+        else
+            render 'past_event'
+        end
+    end
+    
+    def upcoming_events(upcoming_events)
+        if upcoming_events.all.size.zero?
+            render 'no_upcoming_events'
+        else
+            render 'future_events'
+        end
+    end
+    
+    def attendees
+        if @event.attendees.all.size.zero?
+          render 'no_attendance'
+        else
+          render 'attendance', event: @event
+    end
 end
