27a5d4c307eeeeb8ab035ccc552c6aa3207d4a39
Jebitok <sharonkosgei4@gmail.com>
2021-03-08 15:31:34 +0300

    Add Changes to Event Controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,37 +1,38 @@
 class EventsController < ApplicationController
-  # before_action only: %i[ create ]
-
-  # GET /events or /events.json
   def index
     @events = Event.all
+    @past_events = Event.past
+    @upcoming_events = Event.future
   end
 
-  # GET /events/1 or /events/1.json
   def show
     @event = Event.find(params[:id])
+    @users = User.all
   end
 
-  # GET /events/new
   def new
-    @user = User.find(params[:user_id])
     @event = Event.new
   end
 
-  # POST /events or /events.json
   def create
-    @user = User.find(params[:user_id])
-    @event = @user.created_events.build(event_params)
-    redirect_to user_path(@user).event
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flass[:info] = 'The event was created successfully.'
+      redirect_to events_path
+    else
+      flash[:info] = @events.errors.full_messages
+      render :new
+    end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_event
-    #   @event = Event.find(params[:id])
-    # end
+  def add_user_to_event(user)
+    attendees << user
+    redirect_to user
+  end
+  helper_method :add_user_to_event
 
-    # Only allow a list of trusted parameters through.
+  private
     def event_params
-      params.permit(:event_name, :description, :date, :location)
+      params.require(:event).permit(:event_name, :description, :date, :location)
     end
 end
