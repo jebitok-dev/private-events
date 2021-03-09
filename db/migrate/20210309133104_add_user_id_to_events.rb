dc02ddadaaf941a449171d8939bb5b32988d8478
Jebitok <sharonkosgei4@gmail.com>
2021-03-09 18:28:18 +0300

    Add Changes To Controllers and Migrations

--- db/migrate/20210309133104_add_user_id_to_events.rb
+++ db/migrate/20210309133104_add_user_id_to_events.rb
@@ -1,6 +1,6 @@
 class AddUserIdToEvents < ActiveRecord::Migration[6.1]
   def change
-    add_column :events, :user_id, :integer
+    add_column :events, :user_id, :integer, :default => 0
     add_index :events, :user_id
   end
 end
