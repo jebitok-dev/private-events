e084e47b57afc42db2831a09d42f9a624832679b
Jebitok <sharonkosgei4@gmail.com>
2021-03-09 23:00:15 +0300

    Add Changes to Migrations and Views

--- db/migrate/20210309133104_add_user_id_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToEvents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :user_id, :integer, :default => 0
-    add_index :events, :user_id
-  end
-end
--- db/migrate/20210309152521_add_creator_id_to_events.rb
+++ db/migrate/20210309152521_add_creator_id_to_events.rb
@@ -1,6 +1,6 @@
 class AddCreatorIdToEvents < ActiveRecord::Migration[6.1]
   def change
-    add_column :events, :creator_id, :integer
+    add_column :events, :creator_id, :integer, default => 0
     add_index :events, :creator_id
   end
 end
